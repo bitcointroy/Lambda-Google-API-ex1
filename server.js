21715913f2a6bd9d843b68210643f4db599c598d
Troy Bradley <travlr2010@gmail.com>
2018-02-15 19:11:44 -0600

    Getting the google results through a localhost API

--- server.js
+++ server.js
@@ -2,25 +2,56 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const config = require('./.config.js');
+// const details = require('./data/placeDetails.json');
 
 const server = express();
 const PORT = config.port;
-const GMAPS_KEY = config.gmaps.key;
+const STATUS_SUCCESS = 200;
+const STATUS_USER_ERROR = 422;
+const KEY_GMAPS = config.gmaps.key;
+const URI_TEXT_SEARCH = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
 
-
-
-server.listen(PORT, err => {
-    if (err) {
-        console.log(`Error starting server: ${err}`);
-    } else {
-        console.log(`App listening on port: ${PORT}`);
-    }
+server.get('/place', (req, res) => {
+	const place = req.query.place;
+	const url = URI_TEXT_SEARCH + place + '&key=' + KEY_GMAPS;
+	fetch(url)
+		.then((place) => place.json())
+		.then((place) => {
+			console.log(place);
+			res.status(STATUS_SUCCESS);
+			res.send(place);
+		})
+		.catch((err) => {
+			res.status(STATUS_USER_ERROR);
+			res.send({ err: err });
+		});
 });
 
+// const getLocation = async url => {
+//     try {
+//         const response = await fetch(url);
+//         const json = await response.json();
+//         console.log(json);
+//     }
+// }
+
+server.listen(PORT, (err) => {
+	if (err) {
+		console.log(`Error starting server: ${err}`);
+	} else {
+		console.log(`App listening on port: ${PORT}`);
+	}
+});
+
+// server.get('/places', (req, res) => {
+//     getLocation(search);
+
+// });
 
 server.get('/', (req, res) => {
-    res.send(
-        '<p>Hello Troy!</p>'+
-        '<p>The fun stuff is at ...</p>' // edit to show main folder for calls
-    );
+	res.send(
+		'<p>Hello Troy!</p>' + '<p>The fun stuff is at ...</p>' // edit to show main folder for calls
+	);
 });
+
+// console.log(details.result.opening_hours.weekday_text);
